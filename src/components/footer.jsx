90265afb99db1155ce8490a2e960f8390d7a772d
Patrik Bogno <patrik.bogno@student.tuke.sk>
2025-10-27 12:23:48 +0100

    0.0.9 Icons creation, added first icons to footer, some changes to footer style.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,24 @@
 import { View, Text } from "react-native"
+import { Icons } from "../assets/icons/icons.tsx"
 import style from "../styles/footer_style.js";
 
 function Footer() {
     return (
         <View style={style.container}>
-            <Text style={style.text}>Test</Text>
+            <View style={style.icon_menu_container}>
+                <View style={style.icon_container}>
+                    <Icons.House style={style.icon} />
+                </View>
+                <View style={style.icon_container}>
+                    <Icons.Settings style={style.icon} />
+                </View>
+                <View style={style.icon_container}>
+                    <Icons.Edit style={style.icon} />
+                </View>
+                <View style={style.icon_container}>
+                    <Icons.Book style={style.icon} />
+                </View>
+            </View>
         </View>    
     );
 }
--- src/styles/footer_style.js
+++ src/styles/footer_style.js
@@ -1,18 +1,36 @@
+import jsxRuntime from 'react/jsx-runtime';
 import global_style from './global_style';
 
 var React = require('react-native');
 
 var style = React.StyleSheet.create({
     container: {
+        flex: 1,
+        flexDirection: 'row',
         height: '100%',
         width: React.Dimensions.get("screen").width,
         alignItems: 'center',
         justifyContent: 'center',
     },
 
-    text: {
-        color: global_style.colors.text,
-    }
+    icon_menu_container: {
+        width: '80%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },  
+
+    icon_container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    icon: {
+        flex: 1,
+        width: 40,
+        height: 40,
+    },  
 })
 
 module.exports = style;
